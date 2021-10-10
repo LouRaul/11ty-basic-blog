2beb090f75e14d6a1986e5a961d975571e419f90
Rolo <loureiroraul@gmail.com>
2021-10-10 11:11:25 -0300

    agustin

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,6 @@
 module.exports = function (eleventyConfig) {
+	eleventyConfig.addPassthroughCopy('./src/style.css');
+	eleventyConfig.addPassthroughCopy('./src/assets');
 	// Return your Object options:
 	return {
 		dir: {
